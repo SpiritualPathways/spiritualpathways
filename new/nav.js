b0255328f95711f4f76715b7d7456589d9906f5c
Robert Coffey <spiritualpathwaysnl@gmail.com>
2024-05-15 06:42:50 -0230

    add new website layout

---
+++ new/nav.js
@@ -0,0 +1,5 @@
+const nav = document.getElementById('nav')
+nav.innerHTML =
+	'<div><a href="index.html">Home</a></div>' +
+    '<div><a href="programs.html">Programs</a></div>' +
+    '<div><a href="contact.html">Contact</a></div>';
