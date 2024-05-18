a2271c170647804d9e58ef9026de54c76ee96b4d
Robert Coffey <spiritualpathwaysnl@gmail.com>
2024-05-17 22:01:25 -0230

    add programs page and remove contacts from nav

--- new/nav.js
+++ new/nav.js
@@ -1,5 +1,4 @@
 const nav = document.getElementById('nav')
 nav.innerHTML =
 	'<div><a href="index.html">Home</a></div>' +
-    '<div><a href="programs.html">Programs</a></div>' +
-    '<div><a href="contact.html">Contact</a></div>';
+    '<div><a href="programs.html">Programs</a></div>';
